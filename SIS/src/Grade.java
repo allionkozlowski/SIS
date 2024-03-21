b578f66995cbcbda75ecb6a5b86f3183cd397610
allison.kozlowski <allison.kozlowski@MU-C1-03.mullen.pvt.k12.co.us>
2024-03-21 11:29:17 -0600

    scan

--- SIS/src/Grade.java
+++ SIS/src/Grade.java
@@ -18,6 +18,7 @@ public class Grade
 				System.out.println("What do you want to change the grade to?");
 				String newGrade = scanner1.nextLine();
 				changeGrade(number1,classPeriod,newGrade);
+				PrintStudentNames.display();
 				
 			}
 		public static void changeGrade(int num, int period, String grade)throws IOException
@@ -26,24 +27,6 @@ public class Grade
 			{
 				StudentList.studentList.get(num).setFirstGrade(grade);
 				System.out.println(StudentList.studentList.get(num).getFirstGrade());
-				int num1 = 1;
-				for(int i=0; i<StudentList.studentList.size(); i++)
-				{
-					String number = Integer.toString(num1) + ")";
-					String name = StudentList.studentList.get(i).getFirstName() + " "+ StudentList.studentList.get(i).getLastName();
-					DecimalFormat d = new DecimalFormat("0.00");
-				    String gpa = d.format(StudentList.studentList.get(i).getGpa()); 
-					String firstClass = StudentList.studentList.get(i).getFirstClass();
-					String firstGrade = StudentList.studentList.get(i).getFirstGrade();
-					String secondClass = StudentList.studentList.get(i).getSecondClass();
-					String secondGrade = StudentList.studentList.get(i).getSecondGrade();
-					String thirdClass = StudentList.studentList.get(i).getThirdClass();
-					String thirdGrade = StudentList.studentList.get(i).getThirdGrade();
-					
-					System.out.printf("%-3s %-17s %-5s %-7s %-5s %-7s %-5s %-7s %-5s", number,name,gpa,firstClass,firstGrade,secondClass,secondGrade,thirdClass,thirdGrade);
-					System.out.println();
-					num++;
-				}
 			}
 			else if(period==2)
 			{
